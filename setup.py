a5f51ed3a10677769610688352bf9a5ca80d2e83
AndreiChertkov <andre.chertkov@gmail.com>
2023-02-10 02:23:16 +0300

    Setup script

---
+++ setup.py
@@ -0,0 +1,73 @@
+import os
+import re
+from setuptools import setup
+
+
+def find_packages(package, basepath):
+    packages = [package]
+    for name in os.listdir(basepath):
+        path = os.path.join(basepath, name)
+        if not os.path.isdir(path):
+            continue
+        packages.extend(find_packages('%s.%s'%(package, name), path))
+    return packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+desc = 'Method PROTES (PRobability Optimizer with TEnsor Sampling) for optimization of the multidimensional arrays and  discretized multivariable functions based on the tensor train (TT) format'
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    desc_long = f.read()
+
+
+with open(os.path.join(here, 'protes/__init__.py'), encoding='utf-8') as f:
+    text = f.read()
+    version = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", text, re.M)
+    version = version.group(1)
+
+
+with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().split('\n')
+    requirements = [r for r in requirements if len(r) >= 3]
+
+
+setup_args = dict(
+    name='protes',
+    version=version,
+    description=desc,
+    long_description=desc_long,
+    long_description_content_type='text/markdown',
+    author='Andrei Chertkov',
+    author_email='andre.chertkov@gmail.com',
+    url='https://github.com/anabatsh/PROTES',
+    classifiers=[
+        'Development Status :: 3 - Alpha', # 4 - Beta, 5 - Production/Stable
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='Derivative-free optimization multidimensional optimization low-rank representation tensor train format',
+    packages=find_packages('protes', './protes/'),
+    python_requires='>=3.6',
+    project_urls={
+        'Source': 'https://github.com/anabatsh/PROTES',
+    },
+    license='MIT',
+    license_files = ('LICENSE.txt',),
+)
+
+
+if __name__ == '__main__':
+    setup(
+        **setup_args,
+        install_requires=requirements,
+        include_package_data=True)
